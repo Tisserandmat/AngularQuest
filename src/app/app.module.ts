ff7750e8bcd3b5f572187e996e55da1815722343
Tisserandmat <tisserand.mat@gmail.com>
2021-12-21 21:09:16 +0100

    Quetes angular - Directives 3.1 TERMINER.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DirectivesComponent } from './directives/directives.component';
+import { StylesQuestDirective } from './styles-quest.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DirectivesComponent,
+    StylesQuestDirective
   ],
   imports: [
     BrowserModule
